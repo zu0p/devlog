198473e92eef134341ce1e47d87c792149d55a9a
zu0p <zu0p@kakao.com>
2026-03-12 14:18:51 +0900

    style: Label 컴포넌트 디자인 수정

--- ds/components/atoms/label/Label.tsx
+++ ds/components/atoms/label/Label.tsx
@@ -1,14 +1,21 @@
 import { labelSizeClass } from "@/ds/tokens/label/sizes"
+import { labelVariantClass } from "@/ds/tokens/label/variants"
 import { LabelProps } from "./Label.types"
 import { cn } from "@/lib/utils"
 
-const Label = ({ htmlFor, size, className, ...props }: LabelProps) => {
+const Label = ({
+  htmlFor,
+  size = "sm",
+  variant = "default",
+  className,
+  ...props
+}: LabelProps) => {
   return (
     <label
       htmlFor={htmlFor}
       className={cn(
-        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         labelSizeClass[size],
+        labelVariantClass[variant],
         className
       )}
       {...props}
