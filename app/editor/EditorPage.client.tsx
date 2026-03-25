df3603241c161b3693c100c6782add7c5843b50a
zu0p <zu0p@kakao.com>
2026-03-25 13:02:39 +0900

    chore: 변경된 GeneratePreview 적용

--- app/editor/EditorPage.client.tsx
+++ app/editor/EditorPage.client.tsx
@@ -1,13 +1,12 @@
 "use client"
 import { useGeneratedArticle } from "@/stores/generatedArticle.store"
 import { useRef } from "react"
-import LoadingPage from "./loading"
 import ErrorPage from "./error"
-import EditorHeader from "./components/EditorHeader"
 import ExportActions from "./components/ExportActions"
 import { Editor as ToastEditorType } from "@toast-ui/react-editor"
 import dynamic from "next/dynamic"
-import MetaData from "./components/MetaData"
+import GeneratedPreview from "@/ds/components/organisms/preview/GeneratedPreview"
+import { clipboardCopy } from "@/lib/exportDocuments"
 
 const ToastUIEditor = dynamic(() => import("./components/ToastUIEditor"), {
   ssr: false,
@@ -16,16 +15,39 @@ const ToastUIEditor = dynamic(() => import("./components/ToastUIEditor"), {
 const EditorPage = () => {
   const { isLoading, error } = useGeneratedArticle()
   const editorRef = useRef<ToastEditorType>(null)
+  const { metaDescription, hashtags } = useGeneratedArticle()
 
-  if (isLoading) return <LoadingPage />
+  const handleCopyMetaDescription = () => {
+    if (!metaDescription) return
+
+    clipboardCopy(metaDescription, "SEO Meta Description 복사 완료!")
+  }
+
+  const handleCopyHashTags = () => {
+    if (!hashtags) return
+
+    const joinedHashtags = hashtags.map((hashtag) => `#${hashtag}`).join(" ")
+    clipboardCopy(joinedHashtags, "해시태그 복사 완료!")
+  }
+
   if (error) return <ErrorPage />
 
   return (
     <div className="m-3 space-y-4 md:m-0">
-      <EditorHeader />
-      <MetaData />
-      <ToastUIEditor editorRef={editorRef} />
-      <ExportActions editorRef={editorRef} />
+      <GeneratedPreview
+        isLoading={isLoading}
+        title={"생성된 블로그 글"}
+        content={<ToastUIEditor editorRef={editorRef} />}
+        metaDescription={{
+          click: handleCopyMetaDescription,
+          text: metaDescription!,
+        }}
+        hashtags={{
+          click: handleCopyHashTags,
+          array: hashtags!,
+        }}
+      />
+      {!isLoading && <ExportActions editorRef={editorRef} />}
     </div>
   )
 }
